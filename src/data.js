67c2d0238a38f2cbaa1761624291f54985835e90
Andrey <dovban.andreyy@rambler.ru>
2022-11-06 19:32:18 +0300

    Add sumulale data.json

---
+++ src/data.js
@@ -0,0 +1,141 @@
+const data = [
+  {
+    mark: 'audi',
+    model: 'A4',
+    year: 2015,
+    delivery: '21.05.2021-28.02.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A4',
+    year: 2016,
+    delivery: '01.05.2021-21.03.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A5',
+    year: 2015,
+    delivery: '21.05.2022-28.06.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A5',
+    year: 2016,
+    delivery: '1.05.2022-8.06.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A5',
+    year: 2017,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A6',
+    year: 2015,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A6',
+    year: 2016,
+    delivery: '14.05.2022-17.06.2022'
+  },
+  {
+    mark: 'audi',
+    model: 'A6',
+    year: 2017,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyota',
+    model: 'camry',
+    year: 2015,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyota',
+    model: 'camry',
+    year: 2016,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyaota',
+    model: 'camry',
+    year: 2019,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyota',
+    model: 'supra',
+    year: 2014,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyota',
+    model: 'supra',
+    year: 2013,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'toyaota',
+    model: 'supra',
+    year: 2019,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'lancer',
+    year: 2015,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'lancer',
+    year: 2013,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'lancer',
+    year: 2019,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'outlander',
+    year: 2014,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'outlander',
+    year: 2013,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'outlander',
+    year: 2019,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'eclipce',
+    year: 2012,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'eclipce',
+    year: 2013,
+    delivery: '23.05.2022-27.06.2022'
+  },
+  {
+    mark: 'mitsubishi',
+    model: 'eclipce',
+    year: 2015,
+    delivery: '23.05.2022-27.06.2022'
+  }
+];
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 'use strict';
+// Имитация запроса и присвоение полученных даннах в переменную
+import data from './data.js';
+
+// Начальный квадрат с его четверринками
 const square = document.querySelector('.square');
 const square_tl = document.querySelector('.top-left');
 const square_tr = document.querySelector('.top-right');
 const square_br = document.querySelector('.bottom-right');
 const square_bl = document.querySelector('.bottom-left');
 
+// Круглая кнопка для входа в меню
 const circle = document.querySelector('.circle');
+// Форма
 const form = document.querySelector('.form');
 
+// Ширина и высота области проасмотра
 const wd = window.innerWidth;
 const hd = window.innerHeight;
 
+// Позагруке страницы срабатывает функия onLoad
 window.onload = onLoad;
 
+//
+form.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+// Функция onLoad производит манипуляции с елементами на странице
 function onLoad() {
+  // Четвертинки разбегаются по углам
   setTimeout(() => {
     square_tl.style.transform = `translate(-${wd / 2}px, -${hd / 2}px)`;
     square_tr.style.transform = `translate(${wd / 2}px, -${hd / 2}px)`;
@@ -22,21 +37,27 @@ function onLoad() {
     console.log(1);
   }, 500);
 
+  // Появляется круглая кнопка
   setTimeout(() => {
     circle.style.visibility = 'visible';
     circle.classList.add('rotate');
     console.log(2);
   }, 2500);
 
+  // Кнопка становится рабочей и на неё вешается функция clickToCircle
   setTimeout(() => {
-    circle.classList.add('active');
+    circle.classList.remove('disabled');
+    circle.classList.add('work-button');
     circle.onclick = clikcToCircle;
     console.log(3);
   }, 4500);
 }
 
+// Функция clicToCircle прячеткнопку и показывает форму для выбора автомобиля
 function clikcToCircle() {
   square.classList.add('hide');
   form.classList.remove('hide');
   form.classList.add('flex');
 }
+
+console.log(data);
